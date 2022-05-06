b2f64a1180d04829a0e6f5e1d884e687f5871167
roylin <1002591652@qq.com>
2022-05-06 22:54:25 +0800

    feat: init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import { ConfigProvider } from "antd-mobile";
+import zhCN from "antd-mobile/es/locales/zh-CN";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ConfigProvider locale={zhCN}>
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
 
-export default MyApp
+export default App;
